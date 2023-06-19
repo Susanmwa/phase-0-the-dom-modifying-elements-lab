169ec4cede18537fc94058d1443f1c35ebb69719
Mischel <susan.mwaura@student.moringaschool.com>
2023-06-19 18:34:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+let newHeader = document.querySelector('h1');
+
+let newHeader = document.querySelector('h1#victory');
+
+let newHeader = document.querySelector('h1#victory');
+if (newHeader) {
+    // Set the text content of the 'newHeader' element
+    newHeader.textContent = 'Susan is the champion';
+  }
+
+
+
+
